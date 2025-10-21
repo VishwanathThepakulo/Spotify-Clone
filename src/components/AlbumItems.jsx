d4b11243f43cc1dd63509f75c7d3408c9c86628b
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2025-10-21 08:20:24 +0530

    Added album items

--- src/components/AlbumItems.jsx
+++ src/components/AlbumItems.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const AlbumItems = () => {
+const AlbumItems = (props) => {
   return (
-    <div className=''>
-
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff28]'>
+      <img className='rounded' src={props.imagge} alt="" />
+      <p className='font-bold mt-2 mb-1'>{props.name}</p>
+      <p className='text-slate-200 text-sm'>{props.desc}</p>
     </div>
   )
 }
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumItems from './AlbumItems'
 
 const DisplayHome = () => {
   return (
     <div>
       <Navbar/>
       <div className='my-5 font-bold text-2xl'>
-
+        {albumsData.map((item,index) => (<AlbumItems key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
       </div>
     </div>
   )
