24e8383cc208eaa6e3cdfc719f2595a30ac921b6
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2025-11-10 07:46:32 +0530

    added Album component

--- src/components/AlbumItems.jsx
+++ src/components/AlbumItems.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const AlbumItems = (props) => {
+  const nav = useNavigate()
   return (
-    <div className='min-w-[200px] max-w-[300px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff28]'>
+    <div onClick={()=>nav(`/album/${props.id}`)} className='min-w-[200px] max-w-[300px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff28]'>
       <img className='rounded' src={props.image} alt="" />
       <p className='font-bold mt-2 mb-1'>{props.name}</p>
       <p className='text-slate-200 text-sm'>{props.desc}</p>
